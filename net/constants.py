e721ffb7ff59f588c6ed3ab27f03bf16f7bae31c
Ali Faraj <faraj7g@gmail.com>
2015-11-27 14:19:01 -0200

    deleted setup.py

--- net/constants.py
+++ net/constants.py
@@ -22,11 +22,8 @@ AppStaticHandler = True
 StaticDirs = ["static"]
 
 # Disable debug and enable Sessions
-web.config.debug = False
+#web.config.debug = False
 
 # Databases
 SystemDB = 'sqlite:///test.db'
 UserDB = 'sqlite:///test.db'
-
-
-
