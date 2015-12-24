214d65e9cb382bf4c8b39db3bfb47fae68973b69
Ali Faraj <faraj7g@gmail.com>
2015-12-23 22:24:59 -0200

    student page, student name navbar, change folders and paths

--- net/static/js/scrollable.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function () {
-// passing a fixedOffset param will cause the table header to stick to the bottom of this element
-$("table").stickyTableHeaders({ scrollableArea: $(".scrollable-area")[0]});
-});
